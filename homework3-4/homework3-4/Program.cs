273a3663ccdb4034cc1170cc6ed01befa476c1e5
try1998 <48009583+try1998@users.noreply.github.com>
2019-02-27 20:35:11 +0800

    Add files via upload

---
+++ homework3-4/homework3-4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, result;
+            Console.WriteLine("请输入第一个数字:");
+            num1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入第二个数字:");
+            num2 = Double.Parse(Console.ReadLine());
+            result = num1 * num2;
+            Console.WriteLine(num1 + "*" + num2 +"="+result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homework3-5/homework3-5/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework3_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = Double.Parse(textBox1.Text);
+            double num2 = Double.Parse(textBox2.Text);
+            double result = num1 * num2;
+            this.textBox3.Text = "结果是:"+result;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
