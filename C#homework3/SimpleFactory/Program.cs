9ce94fa5029ca9c3fb8c720098e454a573b6ff9b
try1998 <48009583+try1998@users.noreply.github.com>
2019-03-13 19:22:19 +0800

    Add files via upload

---
+++ C#homework3/SimpleFactory/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    //抽象产品类
+    public interface Chart
+    {
+        void display();
+    }
+
+    //具体产品类:柱状图
+    class HistogramChart : Chart
+    {
+        public HistogramChart()
+        {
+            Console.WriteLine("创建柱状图！");
+        }
+
+        public void display()
+        {
+            Console.WriteLine("显示柱状图！");
+        }
+    }
+
+    //具体产品类:饼状图
+    class PieChart : Chart
+    {
+        public PieChart()
+        {
+            Console.WriteLine("创建饼状图！");
+        }
+
+        public void display()
+        {
+            Console.WriteLine("显示饼状图！");
+        }
+    }
+
+    //具体产品类:折线图
+    class LineChart : Chart
+    {
+        public LineChart()
+        {
+            Console.WriteLine("创建折线图！");
+        }
+
+        public void display()
+        {
+            Console.WriteLine("显示折线图！");
+        }
+    }
+
+    //图标工厂类
+    class ChartFactory
+    {
+        public static Chart getChart(string type)
+        {
+            Chart chart = null;
+            if (type == "histogram") 
+            {
+                chart = new HistogramChart();
+                Console.WriteLine("初始化设置柱状图！");
+            }
+            else if (type=="pie")
+            {
+                chart = new PieChart();
+                Console.WriteLine("初始化设置饼状图！");
+            }
+            else if (type=="line")
+            {
+                chart = new LineChart();
+                Console.WriteLine("初始化设置折线图！");
+            }
+            return chart;
+        }
+    }
+
+    //客户端测试
+    class Client
+    {
+        public static void Main(String []args)
+        {
+            Chart chart;
+            chart = ChartFactory.getChart("histogram"); //通过静态工厂方法创建产品
+            chart.display();
+            chart = ChartFactory.getChart("pie");
+            chart.display();
+            chart = ChartFactory.getChart("line");
+            chart.display();
+            
+        }
+    }
+}
---
+++ C#homework3/Singleton/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class TaskManager
+    {
+        private static TaskManager tm = null;
+        private TaskManager() {} //初始化窗口
+        public void displayProcesses() { Console.WriteLine("显示进程"); } //显示进程
+        public void displayServices() { Console.WriteLine("显示服务"); } //显示服务
+        public static TaskManager getInstance()
+        {
+            if (tm == null)
+            {
+                tm = new TaskManager();
+            }
+            return tm;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskManager instance = TaskManager.getInstance();
+            instance.displayProcesses();
+            instance.displayServices();
+            Console.ReadLine();
+        }
+    }
+}
